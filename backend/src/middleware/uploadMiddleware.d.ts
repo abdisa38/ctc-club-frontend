6868c7e9715899df1cdf5619d6c678e9497bdbe8
Abdisa Awel <abdisaawel313@gmail.com>
2026-05-07 09:40:37 +0300

    feat: add uploadPaymentProof and handleImageUpload functionality

--- backend/src/middleware/uploadMiddleware.d.ts
+++ backend/src/middleware/uploadMiddleware.d.ts
@@ -1,4 +1,5 @@
 import type { RequestHandler } from 'express';
 export declare const handleVideoUpload: RequestHandler;
 export declare const handleResourceUpload: RequestHandler;
+export declare const handleImageUpload: RequestHandler;
 //# sourceMappingURL=uploadMiddleware.d.ts.map
