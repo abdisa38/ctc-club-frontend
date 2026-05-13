cdbfb407eb9041f9afd16b57394241eaa3f916f0
Abdisa Awel <abdisaawel313@gmail.com>
2026-05-13 18:18:16 +0300

    feat:added abdi photo

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -2,7 +2,7 @@ import { motion } from "motion/react";
 import { Link } from "react-router";
 import { ArrowRight, Code2, Award } from "lucide-react";
 import { Button } from "../components/ui/Button";
-import developerPhoto from "../../assets/photo_2026-05-08_09-02-56.jpg";
+import developerPhoto from "../../assets/abdisa-developer-photo.jpg";
 import presidentPhoto from "../../assets/president-shimelis.jpg";
 
 export function About() {
